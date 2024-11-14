46ae1484d43fad74c254f1ab5d1c5c793a6127e6
Siwat Jermwatthana <siwat.jermwatthana@gmail.com>
2024-11-14 16:28:25 +0700

    Feat: Refactor Prisma schema to improve naming conventions and update ID types to UUID

--- prisma/migrations/20241104115518_0_init/migration.sql
+++ prisma/migrations/20241104115518_0_init/migration.sql
@@ -1,5 +1,17 @@
 -- CreateEnum
-CREATE TYPE "USER_STATUS" AS ENUM ('PENDING', 'ACTIVATED');
+CREATE TYPE "UserStatus" AS ENUM ('PENDING', 'ACTIVATED');
+
+-- CreateEnum
+CREATE TYPE "AuthProviderName" AS ENUM ('GOOGLE', 'FACEBOOK', 'GITHUB', 'TWITTER', 'EMAIL');
+
+-- CreateEnum
+CREATE TYPE "ActionType" AS ENUM ('SIGNUP', 'RESETPASSWORD');
+
+-- CreateEnum
+CREATE TYPE "PolicyType" AS ENUM ('PRIVATE', 'TERMOFSERVICES');
+
+-- CreateEnum
+CREATE TYPE "LoginStatus" AS ENUM ('SUCCESS', 'FAILED');
 
 -- CreateTable
 CREATE TABLE "users" (
@@ -10,13 +22,89 @@ CREATE TABLE "users" (
     "username" VARCHAR(50),
     "password" VARCHAR(50),
     "email" VARCHAR(50) NOT NULL,
-    "profile_image_url" VARCHAR(255),
-    "status" "USER_STATUS" NOT NULL,
+    "picture" VARCHAR(255),
+    "status" "UserStatus" NOT NULL,
     "created_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "auth_providers" (
+    "id" UUID NOT NULL,
+    "user_id" UUID,
+    "auth_provider" "AuthProviderName" NOT NULL,
+    "provider_user_id" VARCHAR(50) NOT NULL,
+    "provider_email" VARCHAR(50) NOT NULL,
+    "linked_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "auth_providers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "email_verifications" (
+    "token" VARCHAR(255) NOT NULL,
+    "user_id" UUID NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL,
+    "expired_at" TIMESTAMP(3) NOT NULL,
+    "completed_at" TIMESTAMP(3),
+    "action_type" "ActionType" NOT NULL,
+
+    CONSTRAINT "email_verifications_pkey" PRIMARY KEY ("token")
+);
+
+-- CreateTable
+CREATE TABLE "policies" (
+    "id" UUID NOT NULL,
+    "type" "PolicyType" NOT NULL,
+    "content" TEXT NOT NULL,
+    "version" VARCHAR(5) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "policies_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user_policies" (
+    "id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "policy_id" UUID NOT NULL,
+    "agreed_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "user_policies_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user_login_histories" (
+    "id" UUID NOT NULL,
+    "email" VARCHAR(50) NOT NULL,
+    "login_time" TIMESTAMP(3) NOT NULL,
+    "ip_address" VARCHAR(50) NOT NULL,
+    "location" VARCHAR(50),
+    "user_agent" VARCHAR(50),
+    "status" "LoginStatus" NOT NULL,
+    "failure_reason" VARCHAR(255),
+    "created_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "user_login_histories_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "auth_providers_provider_user_id_key" ON "auth_providers"("provider_user_id");
+
+-- AddForeignKey
+ALTER TABLE "auth_providers" ADD CONSTRAINT "auth_providers_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "email_verifications" ADD CONSTRAINT "email_verifications_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user_policies" ADD CONSTRAINT "user_policies_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user_policies" ADD CONSTRAINT "user_policies_policy_id_fkey" FOREIGN KEY ("policy_id") REFERENCES "policies"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
