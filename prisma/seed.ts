f1744b7c2e89ea1f05da675b9d99cc4be2a7f7e0
Siwat Jermwatthana <siwat.jermwatthana@gmail.com>
2024-11-14 16:29:14 +0700

    Feat: Update seed script to use createMany for bulk user and policy creation

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,14 +5,42 @@ const prisma = new PrismaClient();
 
 const seed = async () => {
   try {
-    await prisma.user.create({
-      data: {
-        name: 'John Doe',
-        username: 'johndoe',
-        password: 'password',
-        email: 'john@email.com',
-        status: 'ACTIVATED',
-      },
+    await prisma.user.createMany({
+      data: [
+        {
+          name: 'John Doe',
+          username: 'johndoe',
+          password: 'password',
+          email: 'john@email.com',
+          status: 'ACTIVATED',
+        },
+        {
+          name: 'Jane Smith',
+          username: 'janesmith',
+          password: 'password',
+          email: 'jane@email.com',
+          status: 'ACTIVATED',
+        },
+      ],
+    });
+
+    await prisma.policy.createMany({
+      data: [
+        {
+          type: 'TERMOFSERVICES',
+          content: 'Termofservices policy',
+          version: '1.0.0',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          type: 'PRIVATE',
+          content: 'Private policy',
+          version: '1.0.0',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
     });
   } catch (error: any) {
     loggerService.error(error);
