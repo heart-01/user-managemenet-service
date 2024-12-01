e0269fc586930764dfe693f4ee39ba1e54fba8b8
Siwat Jermwatthana <siwat.jermwatthana@gmail.com>
2024-12-01 13:11:24 +0700

    Refactor: Remove unnecessary line breaks in getPolicy function

--- src/controllers/policy.controller.ts
+++ src/controllers/policy.controller.ts
@@ -4,10 +4,8 @@ import logger from '../services/logger.service';
 
 export const getPolicy = async (request: Request, response: Response) => {
   logger.start(request);
-
   const result = await policyService.getPolicy();
   response.status(result.status).send(result.data);
-
   logger.end(request);
 };
 
