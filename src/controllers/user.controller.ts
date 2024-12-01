7edc563493d7204a7a8ca90738b58fccf4a7ae25
Siwat Jermwatthana <siwat.jermwatthana@gmail.com>
2024-12-01 13:11:44 +0700

    Refactor: Remove unnecessary line breaks in getUserById function

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -5,12 +5,9 @@ import { GetUserParamType } from '../types/users.type';
 
 export const getUserById = async (request: Request, response: Response) => {
   logger.start(request);
-
   const { id } = request.params as GetUserParamType;
-
   const result = await userService.getUserById(id);
   response.status(result.status).send(result.data);
-
   logger.end(request);
 };
 
