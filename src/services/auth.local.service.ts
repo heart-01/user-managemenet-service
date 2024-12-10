a282f842c87494001bc888c602af781336895e37
Siwat Jermwatthana <siwat.jermwatthana@gmail.com>
2024-12-11 00:41:53 +0700

    Feat: Implement verifyEmailResetPassword and resetPassword functions in auth service

--- src/services/auth.local.service.ts
+++ src/services/auth.local.service.ts
@@ -7,6 +7,7 @@ import {
   ACCESS_TOKEN_EXPIRES_IN,
   CLIENT_URL,
   JWT_SECRET,
+  SENDGRID_TEMPLATE_RESET_PASSWORD_EMAIL,
   SENDGRID_TEMPLATE_VERIFY_EMAIL,
 } from '../config/dotenv';
 import { prisma, Prisma, USER_STATUS, ACTION_TYPE, runTransaction } from '../config/database';
@@ -20,13 +21,17 @@ import {
   VerifyEmailExistMismatchError,
   RecordNotFoundError,
   ResponseError,
+  ResetPasswordMismatchError,
 } from '../errors';
 import { generateToken } from '../utils/token';
 import { hashPassword } from '../utils/hashing';
 import {
   AuthResponseType,
+  PayloadTokenResetPasswordType,
   PayloadTokenVerifyEmailType,
   RegisterType,
+  ResetPasswordResponseType,
+  ResetPasswordType,
   VerifyEmailResponseType,
 } from '../types/auth.type';
 
@@ -64,9 +69,10 @@ const verifyEmailExist = async (
       });
       const isEmailExpired =
         emailVerification && dayjs().isAfter(dayjs(emailVerification.expiredAt));
+      const isTokenCompleted = emailVerification && emailVerification.completedAt;
 
-      // Case email verification is expired
-      if (isEmailExpired) {
+      // Case email verification is expired or token is completed
+      if (isEmailExpired || isTokenCompleted) {
         const token = uuidv4();
         const payload: PayloadTokenVerifyEmailType = { id: emailVerification.userId, token };
         const accessToken = generateToken(payload, JWT_SECRET, tokenExpiresIn);
@@ -83,7 +89,7 @@ const verifyEmailExist = async (
           subject: 'Investnity - Verify your email address',
           templateId: SENDGRID_TEMPLATE_VERIFY_EMAIL,
           dynamicTemplateData: {
-            verificationLink: `${CLIENT_URL}/api/auth/verify?token=${accessToken}`,
+            verificationLink: `${CLIENT_URL}/api/auth/verify/email?token=${accessToken}`,
           },
         });
         return {
@@ -93,7 +99,7 @@ const verifyEmailExist = async (
       }
 
       // Resend email verification
-      if (emailVerification) {
+      if (!isTokenCompleted && !isEmailExpired && emailVerification) {
         const payload: PayloadTokenVerifyEmailType = {
           id: emailVerification.userId,
           token: emailVerification.token,
@@ -104,10 +110,9 @@ const verifyEmailExist = async (
           subject: 'Investnity - Verify your email address',
           templateId: SENDGRID_TEMPLATE_VERIFY_EMAIL,
           dynamicTemplateData: {
-            verificationLink: `${CLIENT_URL}/api/auth/verify?token=${accessToken}`,
+            verificationLink: `${CLIENT_URL}/api/auth/verify/email?token=${accessToken}`,
           },
         });
-
         return {
           status: HTTP_RESPONSE_CODE.CREATED,
           data: emailVerification,
@@ -136,7 +141,7 @@ const verifyEmailExist = async (
         subject: 'Investnity - Verify your email address',
         templateId: SENDGRID_TEMPLATE_VERIFY_EMAIL,
         dynamicTemplateData: {
-          verificationLink: `${CLIENT_URL}/api/auth/verify?token=${accessToken}`,
+          verificationLink: `${CLIENT_URL}/api/auth/verify/email?token=${accessToken}`,
         },
       });
       return {
@@ -160,6 +165,7 @@ const verifyEmailExist = async (
 
 const verifyEmail = async (
   token: string,
+  type: ACTION_TYPE,
 ): Promise<ResponseCommonType<VerifyEmailResponseType | Error>> => {
   try {
     loggerService.info('verifyEmail');
@@ -167,11 +173,7 @@ const verifyEmail = async (
 
     const decoded = verify(token, JWT_SECRET) as PayloadTokenVerifyEmailType;
     const emailVerification = await prisma.emailVerification.findFirst({
-      where: {
-        token: decoded.token,
-        type: ACTION_TYPE.REGISTER,
-        completedAt: null,
-      },
+      where: { token: decoded.token, type, completedAt: null },
     });
 
     // Case token not found
@@ -298,8 +300,192 @@ const register = async (
   }
 };
 
+const verifyEmailResetPassword = async (
+  email: string,
+): Promise<ResponseCommonType<EmailVerification | Error>> => {
+  try {
+    loggerService.info('verifyEmailResetPassword');
+    loggerService.debug('email', email);
+
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    // Case user not found
+    if (!user) {
+      return {
+        status: HTTP_RESPONSE_CODE.NOT_FOUND,
+        data: new RecordNotFoundError('User not found'),
+      };
+    }
+
+    // Case user status is pending
+    if (user && user.status === USER_STATUS.PENDING) {
+      return {
+        status: HTTP_RESPONSE_CODE.CONFLICT,
+        data: new ConflictError('User not activated'),
+      };
+    }
+
+    // Config expiredAt for email verification
+    const expiredAt = dayjs().add(1, 'd').toDate();
+    const tokenExpiresIn = Math.floor((expiredAt.getTime() - Date.now()) / 1000); // Duration in seconds
+
+    // Case user registered status is activated
+    if (user && user.status === USER_STATUS.ACTIVATED) {
+      const emailVerification = await prisma.emailVerification.findFirst({
+        where: {
+          userId: user.id,
+          type: ACTION_TYPE.RESETPASSWORD,
+        },
+        orderBy: {
+          createdAt: 'desc',
+        },
+      });
+      const isEmailExpired =
+        emailVerification && dayjs().isAfter(dayjs(emailVerification.expiredAt));
+      const isTokenCompleted = emailVerification && emailVerification.completedAt;
+
+      // Case email verification not found
+      if (!emailVerification) {
+        const token = uuidv4();
+        const payload: PayloadTokenResetPasswordType = { id: user.id, token };
+        const accessToken = generateToken(payload, JWT_SECRET, tokenExpiresIn);
+        const newEmailVerification = await prisma.emailVerification.create({
+          data: { userId: user.id, token, expiredAt, type: ACTION_TYPE.RESETPASSWORD },
+        });
+        await sendEmailWithTemplate({
+          to: email,
+          subject: 'Investnity - Reset your password',
+          templateId: SENDGRID_TEMPLATE_RESET_PASSWORD_EMAIL,
+          dynamicTemplateData: {
+            verificationLink: `${CLIENT_URL}/api/auth/verify/reset-password?token=${accessToken}`,
+          },
+        });
+        return {
+          status: HTTP_RESPONSE_CODE.CREATED,
+          data: newEmailVerification,
+        };
+      }
+
+      // Case email verification is expired or token is completed
+      if (isEmailExpired || isTokenCompleted) {
+        const token = uuidv4();
+        const payload: PayloadTokenResetPasswordType = { id: emailVerification.userId, token };
+        const accessToken = generateToken(payload, JWT_SECRET, tokenExpiresIn);
+        const newEmailVerification = await prisma.emailVerification.create({
+          data: {
+            userId: emailVerification.userId,
+            token,
+            expiredAt,
+            type: ACTION_TYPE.RESETPASSWORD,
+          },
+        });
+        await sendEmailWithTemplate({
+          to: email,
+          subject: 'Investnity - Reset your password',
+          templateId: SENDGRID_TEMPLATE_RESET_PASSWORD_EMAIL,
+          dynamicTemplateData: {
+            verificationLink: `${CLIENT_URL}/api/auth/verify/reset-password?token=${accessToken}`,
+          },
+        });
+        return {
+          status: HTTP_RESPONSE_CODE.CREATED,
+          data: newEmailVerification,
+        };
+      }
+
+      // Resend email verification
+      if (!isTokenCompleted && !isEmailExpired && emailVerification) {
+        const payload: PayloadTokenResetPasswordType = {
+          id: emailVerification.userId,
+          token: emailVerification.token,
+        };
+        const accessToken = generateToken(payload, JWT_SECRET, tokenExpiresIn);
+        await sendEmailWithTemplate({
+          to: email,
+          subject: 'Investnity - Reset your password',
+          templateId: SENDGRID_TEMPLATE_RESET_PASSWORD_EMAIL,
+          dynamicTemplateData: {
+            verificationLink: `${CLIENT_URL}/api/auth/verify/reset-password?token=${accessToken}`,
+          },
+        });
+        return {
+          status: HTTP_RESPONSE_CODE.CREATED,
+          data: emailVerification,
+        };
+      }
+    }
+
+    return {
+      status: HTTP_RESPONSE_CODE.INTERNAL_SERVER_ERROR,
+      data: new ResetPasswordMismatchError(),
+    };
+  } catch (error) {
+    const err = error as Error;
+    return {
+      status: HTTP_RESPONSE_CODE.INTERNAL_SERVER_ERROR,
+      data: new ResponseError(err.message),
+    };
+  }
+};
+
+const resetPassword = async (
+  user: ResetPasswordType,
+): Promise<ResponseCommonType<ResetPasswordResponseType | Error>> => {
+  try {
+    loggerService.info('resetPassword');
+    loggerService.debug('user', user);
+
+    if (user.password !== user.confirmPassword) {
+      return {
+        status: HTTP_RESPONSE_CODE.BAD_REQUEST,
+        data: new InvalidDataError('Password and Confirm Password do not match.'),
+      };
+    }
+
+    const hashedPassword = await hashPassword(user.password);
+    const result: UserType = await prisma.user.update({
+      where: { id: user.userId },
+      data: {
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+        name: true,
+        phoneNumber: true,
+        bio: true,
+        username: true,
+        email: true,
+        imageUrl: true,
+        status: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+
+    return {
+      status: HTTP_RESPONSE_CODE.OK,
+      data: { user: result },
+    };
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === 'P2025') {
+        return {
+          status: HTTP_RESPONSE_CODE.NOT_FOUND,
+          data: new RecordNotFoundError('User not found'),
+        };
+      }
+    }
+    return {
+      status: HTTP_RESPONSE_CODE.INTERNAL_SERVER_ERROR,
+      data: error as Error,
+    };
+  }
+};
+
 export default {
   verifyEmailExist,
   verifyEmail,
   register,
+  verifyEmailResetPassword,
+  resetPassword,
 };
