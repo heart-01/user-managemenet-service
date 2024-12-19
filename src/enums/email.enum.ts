3ae29c3c2766d8ca5a4b2f675ee2e026e169854d
Siwat Jermwatthana <siwat.jermwatthana@gmail.com>
2024-12-19 10:51:01 +0700

    Feat: Update ResetPassword subject and add ChangePassword subject for email notifications

--- src/enums/email.enum.ts
+++ src/enums/email.enum.ts
@@ -1,4 +1,5 @@
 export enum EmailSubject {
   Register = 'Investnity - Verify your email address',
-  ResetPassword = 'Investnity - Reset your password',
+  ResetPassword = 'Invesnity - Request to reset your password',
+  ChangePassword = 'Investnity - Password changed',
 }
