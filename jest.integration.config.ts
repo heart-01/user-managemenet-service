179083fbb252222c89e5a1ebd7124f3396b88d09
Siwat Jermwatthana <siwat.jermwatthana@gmail.com>
2025-01-06 17:44:01 +0700

    Feat: Update Jest configuration to include module paths and coverage ignore patterns

--- jest.integration.config.ts
+++ jest.integration.config.ts
@@ -3,4 +3,17 @@ export default {
   testEnvironment: 'node',
   testRegex: '((\\.|/*.)(integration.test))\\.ts?$',
   clearMocks: true,
+  modulePaths: ['<rootDir>/src/services/'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '<rootDir>/src/config',
+    '<rootDir>/src/dto',
+    '<rootDir>/src/enums',
+    '<rootDir>/src/utils',
+    '<rootDir>/src/services/aws.service.ts',
+    '<rootDir>/src/services/file.service.ts',
+    '<rootDir>/src/services/index.ts',
+    '<rootDir>/src/services/logger.service.ts',
+    '<rootDir>/src/services/vendor.service.ts',
+  ],
 };
