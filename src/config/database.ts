cc0d442e53e72d1da9117c40b378a341ad894ee5
Siwat Jermwatthana <siwat.jermwatthana@gmail.com>
2025-01-16 21:25:21 +0700

    Refactor: Simplify database configuration by removing unused exports

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,9 +1,11 @@
-import { Prisma, PrismaClient, AUTH_PROVIDER_NAME, USER_STATUS, ACTION_TYPE } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 const TRANSACTION_TIMEOUT = 5000; // 5 seconds
 
-export { Prisma, AUTH_PROVIDER_NAME, USER_STATUS, ACTION_TYPE };
-export const prisma = new PrismaClient();
-export const runTransaction = <T>(
+const prisma = new PrismaClient();
+
+const runTransaction = <T>(
   fn: (prismaTransaction: Prisma.TransactionClient) => Promise<T>,
 ): Promise<T> => prisma.$transaction(fn, { timeout: TRANSACTION_TIMEOUT });
+
+export { Prisma, prisma, runTransaction };
