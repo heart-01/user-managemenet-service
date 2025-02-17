e284e3bcfe71a891dc785aa9af4527327015bdd5
Siwat Jermwatthana <siwat.jermwatthana@gmail.com>
2025-02-17 18:41:57 +0700

    Feat: Add LoginDevice email subject for new device sign-in notifications

--- src/enums/email.enum.ts
+++ src/enums/email.enum.ts
@@ -2,4 +2,5 @@ export enum EmailSubject {
   Register = 'Investnity - Verify your email address',
   ResetPassword = 'Invesnity - Request to reset your password',
   ChangePassword = 'Investnity - Password changed',
+  LoginDevice = 'Investnity - Sign in from new device',
 }
